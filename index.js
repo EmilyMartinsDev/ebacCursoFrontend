5c227a44d45e392d55ddd8280fe4494684878db1
Emily <emilycaroline01042005@gmail.com>
2023-04-07 16:27:25 -0300

    fix: corrigindo bug mensagem-error

--- index.js
+++ index.js
@@ -7,49 +7,64 @@ const error = document.querySelectorAll(".error-message")
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    const campoA = parseFloat(input_campoA.value);
-    const campoB = parseFloat(input_campoB.value);
 
-    if(!campoA && !campoB){
-       error[0].style.display = 'block';
-       error[1].style.display = 'block';
-  
-
-       return;
-
-    }    
-    if(!campoA){
-        error[0].style.display = 'block';
+        let campoA = parseFloat(input_campoA.value);
+        let campoB = parseFloat(input_campoB.value);
     
-        return;
-     }
+        if(!campoA && !campoB){
+           error[0].style.display = 'block';
+           error[1].style.display = 'block';
+      
+    
+           return;
+    
+        }    
+        if(!campoA){
+            error[0].style.display = 'block';
+        
+            return;
+         }
+         
+        if(!campoB){
+            error[1].style.display = 'block';
+    
+            return;
+         }
      
-    if(!campoB){
-        error[1].style.display = 'block';
+    
+        const isValid =  validaForm(campoA, campoB);
+    
+        exibeResultado(isValid);
+    
+        input_campoA.addEventListener('change', (e)=>{
+            campoA = e.target.value;
+            error[0].style.display = 'none';
+            error[1].style.display = 'none';
+        
+            const isValid =  validaForm(campoA, campoB);
+    
+            exibeResultado(isValid);
 
-        return;
-     }
- 
 
-    const isValid =  validaForm(campoA, campoB);
+        
+        });
+        
+        
+        input_campoB.addEventListener('change', (e)=>{
+            campoB = e.target.value;
 
-    exibeResultado(isValid);
+            error[0].style.display = 'none';
+            error[1].style.display = 'none';
+
+
+            const isValid =  validaForm(campoA, campoB);
+    
+            exibeResultado(isValid);
+        
+        });
+        
 });
 
-input_campoA.addEventListener('change', ()=>{
-
-    error[0].style.display = 'none';
-    error[1].style.display = 'none';
-
-});
-
-
-input_campoB.addEventListener('change', ()=>{
-
-    error[0].style.display = 'none';
-    error[1].style.display = 'none';
-
-});
 
 
 
@@ -59,12 +74,16 @@ input_campoB.addEventListener('change', ()=>{
 
  function exibeResultado(isValid){
     if(!isValid){
+        resultado.classList.remove('valid');
+
         resultado.classList.add("invalid");
         resultado.innerHTML = "Formulário invalido, pois, B não é maior que A" ;   
         return; 
 
     }else{
+        resultado.classList.remove('invalid');
         resultado.classList.add("valid");
         resultado.innerHTML = "Formulário valido, pois, B é maior que A" ; 
+        
     }
  }
